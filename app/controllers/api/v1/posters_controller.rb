896ac2d64999530bf420ffbdb34a800363c32ef5
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-04-08 13:48:59 -0700

    fixed uninitialized constant (bug)

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,25 +1,26 @@
 module Api
   module V1
     class PostersController < ApplicationController
+      require_relative '../../../serializers/poster_serializer'
       def index
         posters = Poster.all
-        render json: PosterSerializer.format_posters(posters)
+        render json: ::PosterSerializer.format_posters(posters)
       end
 
       def show
         poster = Poster.find(params[:id])
-        render json: PosterSerializer.format_single_poster(poster)
+        render json: ::PosterSerializer.format_single_poster(poster)
       end
 
       def create
         poster = Poster.create(poster_params)
-        render json: PosterSerializer.format_single_poster(poster)
+        render json: ::PosterSerializer.format_single_poster(poster)
       end
 
       def update
         poster = Poster.find_by(id: params[:id])
         poster.update(poster_params)
-        render json: PosterSerializer.format_single_poster(poster)
+        render json: ::PosterSerializer.format_single_poster(poster)
       end
     
       def destroy
