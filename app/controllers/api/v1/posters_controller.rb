332257b6449c916dc6db1e1550b960c1c94c740f
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-04-09 15:16:12 -0600

    Filter name

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -37,18 +37,31 @@ module Api
       end
 
       def sorted_posters
-        if params[:sort]
-          case params[:sort].downcase
-          when "asc" 
-            Poster.order(created_at: :asc)
-          when "desc" 
-            Poster.order(created_at: :desc)
-          else
-            Poster.all
-          end
-          else
-            Poster.all
+        posters = Poster.all 
+
+        if params[:name]
+          search_name = "%#{params[:name].downcase}%"
+          posters = posters.where("LOWER(name) LIKE ?", search_name).order(:name)
         end
+
+        if params[:min_price]
+          
+        end
+
+        if params[:max_price]
+          
+        end
+
+        if params[:sort] && !params[:name]
+          case params[:sort].downcase
+          when "asc"
+            posters = posters.order(created_at: :asc)
+          when "desc"
+            posters = posters.order(created_at: :desc)
+          end
+        end
+      
+        posters
       end
     end
   end
